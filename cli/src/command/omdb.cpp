ce717e4e64604bd37c4d1ac3587f02055729f7d5
Karn Kaul <karnkaul@gmail.com>
2026-03-24 17:53:41 -0700

    Genericize `command::Movie`

--- cli/src/command/omdb.cpp
+++ cli/src/command/omdb.cpp
@@ -1,24 +1,47 @@
 #include "command/omdb.hpp"
+#include "djson/json.hpp"
 #include "klib/args/arg.hpp"
+#include "klib/debug/assert.hpp"
+#include "log.hpp"
 #include "state/format.hpp"
-#include "vifo/formatter/movie.hpp"
 #include "vifo/types.hpp"
 #include "vifo/util/util.hpp"
+#include <format>
 
 namespace vifo::cli::command {
-void Movie::populate_args() {
+OmdbBase::OmdbBase(omdb::IService const& omdb_service, std::string_view name) : m_omdb_service(&omdb_service), m_command_name(name) {
+	m_command_help = std::format("format a {} directory", m_command_name);
+	m_json_help = std::format("path to json specifying {} format", m_command_name);
+	m_directory_help = std::format("path to {} directory", m_command_name);
+}
+
+void OmdbBase::populate_args() {
 	m_args = {
-		klib::args::positional_required(m_directory, "DIRECTORY", "path to movie directory"),
+		klib::args::named_option(m_format_json, "f,format-json", m_json_help),
+		klib::args::positional_optional(m_directory, "DIRECTORY", m_directory_help),
 	};
 }
 
-auto Movie::execute() -> ExitCode {
+auto OmdbBase::execute() -> ExitCode {
 	auto directory = util::path_if_directory(m_directory);
 	if (directory.empty()) { return ExitCode::InvalidArgument; }
 
-	auto formatter = formatter::Movie::create(*m_omdb_service);
-	if (!formatter) { return handle_error(formatter.error()); }
+	if (!m_format_json.empty()) {
+		auto json = dj::Json::from_file(m_format_json);
+		if (!json) {
+			std::println(stderr, "failed to read format json: '{}'", m_format_json);
+			return ExitCode::IoError;
+		}
+		m_json = std::move(*json);
+		read_from_json();
+		auto const serialized = m_json.serialize({.flags = dj::SerializeFlag::NoSpaces});
+		log.info("read {} format from json: {}\n{}", m_command_name, m_format_json, serialized);
+	}
 
-	return execute_state_machine(std::make_unique<StateFormat>(*formatter, std::move(directory)));
+	auto const result = create_formatter();
+	if (result != ExitCode::Success) { return result; }
+	KLIB_ASSERT(m_formatter);
+
+	return execute_state_machine(std::make_unique<StateFormat>(*m_formatter, std::move(directory)));
 }
 } // namespace vifo::cli::command
--- cli/src/command/omdb.hpp
+++ cli/src/command/omdb.hpp
@@ -1,27 +1,80 @@
 #pragma once
 #include "command/command.hpp"
+#include "djson/json.hpp"
 #include "klib/ptr.hpp"
+#include "vifo/formatter/formatter.hpp"
+#include "vifo/formatter/movie.hpp"
+#include "vifo/formatter/season.hpp"
+#include "vifo/formatter/series.hpp"
+#include "vifo/json_io.hpp"
 #include "vifo/omdb.hpp"
+#include "vifo/types.hpp"
 #include <string_view>
 
 namespace vifo::cli::command {
-class Movie : public Command {
+class OmdbBase : public Command {
   public:
-	explicit Movie(omdb::IService const& omdb_service) : m_omdb_service(&omdb_service) {}
+	explicit OmdbBase(omdb::IService const& omdb_service, std::string_view name);
 
-  private:
-	static constexpr std::string_view name_v{"movie"};
-	static constexpr std::string_view help_v{"format a movie directory"};
+  protected:
+	template <typename FormatterT>
+	[[nodiscard]] static constexpr auto to_name() -> std::string_view {
+		if constexpr (std::same_as<FormatterT, formatter::Movie>) {
+			return "movie";
+		} else if constexpr (std::same_as<FormatterT, formatter::Season>) {
+			return "season";
+		} else if constexpr (std::same_as<FormatterT, formatter::Series>) {
+			return "series";
+		} else {
+			return "unknown";
+		}
+	}
 
-	[[nodiscard]] auto get_name() const -> std::string_view final { return name_v; }
-	[[nodiscard]] auto get_help() const -> std::string_view final { return help_v; }
-
-	void populate_args() final;
-
-	[[nodiscard]] auto execute() -> ExitCode final;
+	virtual void read_from_json() = 0;
+	virtual auto create_formatter() -> ExitCode = 0;
 
 	klib::Ptr<omdb::IService const> m_omdb_service{};
+	std::unique_ptr<IFormatter> m_formatter{};
 
-	std::string_view m_directory{};
+	dj::Json m_json{};
+
+  private:
+	[[nodiscard]] auto get_name() const -> std::string_view final { return m_command_name; }
+	[[nodiscard]] auto get_help() const -> std::string_view final { return m_command_help; }
+	void populate_args() final;
+	auto execute() -> ExitCode final;
+
+	[[nodiscard]] auto execute_stub(IFormatter& formatter) -> ExitCode;
+
+	std::string_view m_command_name{};
+	std::string m_command_help{};
+	std::string m_json_help{};
+	std::string m_directory_help{};
+
+	std::string_view m_format_json{};
+	std::string_view m_directory{"."};
 };
+
+template <std::derived_from<IFormatter> FormatterT>
+class Omdb : public OmdbBase {
+  public:
+	explicit Omdb(omdb::IService const& omdb_service) : OmdbBase(omdb_service, to_name<FormatterT>()) {}
+
+  private:
+	void read_from_json() final {
+		// TODO
+		// from_json(m_json, m_format);
+	}
+
+	auto create_formatter() -> ExitCode final {
+		auto formatter = FormatterT::create(*m_omdb_service, m_format);
+		if (!formatter) { return handle_error(formatter.error()); }
+		m_formatter = std::make_unique<FormatterT>(std::move(*formatter));
+		return ExitCode::Success;
+	}
+
+	FormatterT::Format m_format{};
+};
+
+using Movie = Omdb<formatter::Movie>;
 } // namespace vifo::cli::command
